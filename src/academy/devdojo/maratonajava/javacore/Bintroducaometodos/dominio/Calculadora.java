357e16072d03e07c2cb65ed861cfc3cdb9f59cf1
Mário sérgio <mario-serginho@hotmail.com>
2025-07-21 22:23:22 -0300

    Merge branch 'main' of https://github.com/serginhokeyblade/Estudos-Java

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -10,7 +10,15 @@ public class Calculadora {
         System.out.println(21 - 2);
     }
 
-    public void multiplicaDoisNumeros(int num1, float num2){
+    public void multiplicaDoisNumeros(int num1, float num2) {
         System.out.println(num1 * num2);
+
+    }
+
+    public double divideDoisNumero(double num1, double num2) {
+        if(num2 == 0){
+            return 0;
+        }
+        return num1/num2;
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTeste03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTeste03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumero(20,0);
+        System.out.println(result);
+
+    }
+}
